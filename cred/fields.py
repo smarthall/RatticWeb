f56fcd0531f623b1f6ce885b8ebacb0261e5b485
Daniel Hall <daniel@danielhall.me>
2014-11-20 23:15:56 +1100

    A field for storing JSON in the database

--- cred/fields.py
+++ cred/fields.py
@@ -1,8 +1,10 @@
-from django.db.models import FileField
+from django.db.models import FileField, TextField, SubfieldBase
 from django import forms
 from django.template.defaultfilters import filesizeformat
 from django.utils.translation import ugettext_lazy as _
 from south.modelsinspector import add_introspection_rules
+from django.core.serializers.json import DjangoJSONEncoder
+import json
 
 
 add_introspection_rules([], ["^cred\.fields\.SizedFileField"])
@@ -27,3 +29,36 @@ class SizedFileField(FileField):
             pass
 
         return data
+
+
+class JSONField(TextField):
+    """JSONField is a generic textfield that neatly serializes/unserializes
+    JSON objects seamlessly"""
+
+    # Used so to_python() is called
+    __metaclass__ = SubfieldBase
+
+    def to_python(self, value):
+        """Convert our string value to JSON after we load it from the DB"""
+
+        if value == "":
+            return None
+
+        try:
+            if isinstance(value, basestring):
+                return json.loads(value)
+        except ValueError:
+            pass
+
+        return value
+
+    def get_db_prep_save(self, value):
+        """Convert our JSON object to a string before we save"""
+
+        if value == "":
+            return None
+
+        if isinstance(value, dict):
+            value = json.dumps(value, cls=DjangoJSONEncoder)
+
+        return super(JSONField, self).get_db_prep_save(value)
